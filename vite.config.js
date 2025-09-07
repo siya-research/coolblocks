86fef08262c8c6e8f8c0f9250981cfd803d4544f
siya-research <siya.briar.sharma@gmail.com>
2025-09-07 19:45:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/coolblocks/", // matches your repo name exactly
+  base: "/coolblocks/", // EXACTLY your repo name
 });
